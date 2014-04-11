775b737fab52bda4058208caf1efa9bba2f83796
Jonathan Duarte <jonathan.duarte@rocketmail.com>
2014-04-11 16:48:34 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jogarforca::Application.config.secret_key_base = '2d562cf3a06cac86a48724937d5a646fa88b197403468fc27479c39b5b2280fb69bc9b28d301b8b25bcb758cc289df691f426bb22101befcd92a3df66b0e290e'
