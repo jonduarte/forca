fc987e2fb1fb0b29007c8cf0a1bbb88b3806e040
Jonathan Duarte <jonathan.duarte@rocketmail.com>
2014-04-13 18:00:27 -0300

    Cleanup views using helpers

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,14 @@
 module GamesHelper
+  def disabled?(game, letter)
+    game.win? || game.guessed?(letter)
+  end
+
+  def button_letter(game, letter)
+    button_to letter,
+      game_path(game, letter: letter),
+      method: :patch,
+      disabled: disabled?(game, letter),
+      form: { id: "form_#{letter}" },
+      remote: true
+  end
 end
